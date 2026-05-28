eee8ca71c0bb5d52b39d849ee1e81c7bf80f324a
Agustin Hernandez Reynoso <reynosoagustin100@gmail.com>
2026-05-28 14:51:09 -0700

    Stanely Conroller In Progress

--- StanelyController/main.py
+++ StanelyController/main.py
@@ -37,19 +37,9 @@ sys.path.insert(1,'C:\\Users\\ejahi\\carla\\PythonAPI\\carla')
 
 from agents.navigation.global_route_planner import GlobalRoutePlanner
 
-##################### Provided by CARLA Documenation ###########################
-def visualize_spawn_points_on_map(world, spawnPoints):
-    for i, spawn_point in enumerate(spawnPoints):
-        # Draw in the spectator window the spawn point index
-        world.debug.draw_string(spawn_point.location, str(i), life_time=100)
-        # We can also draw an arrow to see the orientation of the spawn point
-        # (i.e. which way the vehicle will be facing when spawned)
-        world.debug.draw_arrow(spawn_point.location, spawn_point.location + spawn_point.get_forward_vector(), life_time=100)
-
-################################# Custom #######################################
 
 class PIDControl:
-    def __init__(self, Kp, Ki, Kd):
+    def __init__(self, Kp, Ki, Kd, ff):
         self.Kp = Kp
         self.Ki = Ki
         self.Kd = Kd
@@ -70,7 +60,7 @@ class PIDControl:
 
         self.previousError = error 
 
-        u =  porportionalTerm + integralTerm + derivativeTerm
+        u =  porportionalTerm + integralTerm + derivativeTerm + ff
 
         self.previousTime = currentTime
 
@@ -81,9 +71,85 @@ class PIDControl:
         
         return u
 
-class stanleyController:
-    def __init(self):
-        pass
+class Stanley:
+    def __init__(self):
+        self.wpi = 0
+
+    def closest_path_point(self, F, xy_path):
+        N = len(xy_path)
+        idx = min(self.wpi, N - 2)
+        wp1 = xy_path[idx]
+        wp2 = xy_path[idx + 1]
+        v = wp2 - wp1
+        v_mag = np.linalg.norm(v)
+        v_uv = v / v_mag
+
+        s = (F-wp1) @ v_uv
+        
+        if abs(s) >= v_mag and self.wpi < N-2:
+            self.wpi += 1
+                
+        closestPoint = wp1  + v_uv * s
+        crossTrackError = F - closestPoint
+
+        ############ REMOVE AFTER ##################
+        print(" ")
+        print("-------------------------------------------")
+        print("Closest Path Point Status")
+        print("-------------------------------------------")
+        print(f"Current Location: {F}")
+        print(f"WP1[{idx}]: {wp1} WP2[{idx+1}]: {wp2}")
+        print(f"WP2-WP1: {v}")
+        print(f"WP2-WP1 Magnitude: {v_mag}")
+        print(f"Unit Vector of WP2-WP1: {v_uv}")
+        print(f"Path Heading: { np.arctan2(v_uv[1], v_uv[0])}")
+        print(f"Closest Point: {closestPoint}")
+        print(f"Cross Track Error: {crossTrackError}, Magnitude {np.linalg.norm(crossTrackError)}")
+        print("-------------------------------------------")
+        #############################################
+
+        return crossTrackError, np.arctan2(v_uv[1], v_uv[0])
+    
+    def controller(self,crossTrackError, pathHeading, vehicleHeading, speed, k):
+        headingTerm = pathHeading - vehicleHeading
+        crossTrackAngle = np.arctan2(crossTrackError[1], crossTrackError[0])
+        crossTrackErrorSign = math.copysign(1,crossTrackAngle)
+        crossTrackErrorMagnitude = np.linalg.norm(crossTrackError)
+
+        crossTrackTerm = np.arctan2(k*crossTrackErrorMagnitude*crossTrackErrorSign,speed)
+        steer = headingTerm + crossTrackTerm
+        steer = np.clip(steer,-1.0,1.0)
+
+        ############ REMOVE AFTER ##################
+        print(" ")
+        print("-------------------------------------------")
+        print("Controller Status")
+        print("-------------------------------------------")
+        print(f"Path Heading: {pathHeading} Vehicle Heading: {vehicleHeading}")
+        print(f"Heading Term: {headingTerm}")
+        print(f"Cross Track Error: {crossTrackError}")
+        print(f"Cross Track Error Angle: {crossTrackAngle}")
+        print(f"Cross Track Error Sign: {crossTrackErrorSign}")
+        print(f"Cross Track Error Magnitude: {crossTrackErrorMagnitude}")
+        print(f"Cross Track Term: {crossTrackTerm}")
+        print(f"Steer Command: {steer}")
+        print("-------------------------------------------")
+        #############################################
+
+        return steer
+
+    
+
+##################### Provided by CARLA Documenation ###########################
+def visualize_spawn_points_on_map(world, spawnPoints):
+    for i, spawn_point in enumerate(spawnPoints):
+        # Draw in the spectator window the spawn point index
+        world.debug.draw_string(spawn_point.location, str(i), life_time=100)
+        # We can also draw an arrow to see the orientation of the spawn point
+        # (i.e. which way the vehicle will be facing when spawned)
+        world.debug.draw_arrow(spawn_point.location, spawn_point.location + spawn_point.get_forward_vector(), life_time=100)
+
+################################# Custom #######################################
 
 def intialize_specator(world,vehicle):
     try:
@@ -111,19 +177,21 @@ def draw_way_points(world,waypoints):
                                 persistent_lines=True)
         
 ################################## Main ########################################
-simulationTime = 8.0
+
+simulationTime = 3.0
 startPoint = None
 endPoint = None
 sampleResolutoin = 2
 
-# PID gains and vref
+# PID paremters 
+vref= 12.0
 Kp = 1.2
 Ki = 0.03
 Kd = 0.01
-
-vref= 15.0
+ff = 0.0
 
 # Stanely gain
+k = 5.0
 
 def main():
 
@@ -132,6 +200,10 @@ def main():
     vehicle = None 
     map = None
     waypoints = []
+    xy_waypoints = []
+    velocityAxis = []
+    errorAxis = []
+    timeAxis = []
 
     try:
         # Lets connect to the Carla server
@@ -150,8 +222,8 @@ def main():
         if not spawn_points:
             print("Error: No spawn points found")
             return 
-        startPoint = spawn_points[38] #50
-        endPoint = spawn_points[364]
+        startPoint = spawn_points[50] #50
+        endPoint = spawn_points[100] # 100
 
         # # Choosing Vehicle
         blueprint_library = world.get_blueprint_library()
@@ -171,19 +243,28 @@ def main():
         routePlanner = GlobalRoutePlanner(map,sampleResolutoin)
         waypoints = create_way_points(world,routePlanner,startPoint,endPoint)
         draw_way_points(world, waypoints)
+        previousPointX = 0
+        previousPointY = 0
+        for w in waypoints:
+            if ((w.x != previousPointX) & (w.y != previousPointY)):
+                xy_waypoints.append([w.x,w.y])
+                previousPointX = w.x
+                previousPointY = w.y
+        xy_waypoints = np.array(xy_waypoints)
 
         # Controlling vehicle 
         vehicleControl = carla.VehicleControl()
 
-        # PID 
-        pid = PIDControl(Kp,Ki,Kd)
+        # PID Controller
+        pid = PIDControl(Kp,Ki,Kd,ff)
         pid.previousTime = time.perf_counter()
-        # # Main Loop
-        initialTime = time.time()
 
-        velocityAxis = []
-        errorAxis = []
-        timeAxis = []
+        # Stanley 
+        stanley = Stanley()
+
+        # Main Loop
+        initialTime = time.time()
+        
         while (time.time() - initialTime) < simulationTime:
             
             # Will be use for time-plot
@@ -195,6 +276,13 @@ def main():
             vehicleControl.throttle = u
             vehicle.apply_control(vehicleControl)
 
+            # Stanely
+            vehicle_transform = vehicle.get_transform()
+            vehicleHeading = math.radians(vehicle_transform.rotation.yaw)
+            currentLocation = np.array([vehicle.get_location().x, vehicle.get_location().y])
+            crossTrackError, pathHeading = stanley.closest_path_point(currentLocation, xy_waypoints)
+            steering = stanley.controller(crossTrackError,pathHeading,vehicleHeading,currentVelocity,k)
+            vehicleControl.steer = steering
 
             # Updates camera to stay near vehicle 
             intialize_specator(world,vehicle)
@@ -214,26 +302,25 @@ def main():
         timeAxis = np.array(timeAxis)
 
         # Plot Settings
-
-        plt.subplot(2,1,1)
-        plt.xlabel("Time (s)")
-        plt.ylabel("Vehicle Speed (m/s)")
-        plt.title("PID Controller Results")
-        plt.axhline(y=vref,color='gray',linestyle='-')
-        plt.minorticks_on()
-        plt.grid(True, which="both", linewidth = 0.2)
-        plt.plot(timeAxis, velocityAxis, linewidth="2")
+        # plt.subplot(2,1,1)
+        # plt.xlabel("Time (s)")
+        # plt.ylabel("Vehicle Speed (m/s)")
+        # plt.title("PID Controller Results")
+        # plt.axhline(y=vref,color='gray',linestyle='-')
+        # plt.minorticks_on()
+        # plt.grid(True, which="both", linewidth = 0.2)
+        # plt.plot(timeAxis, velocityAxis, linewidth="2")
         
-        plt.subplot(2,1,2)
-        plt.xlabel("Time (s)")
-        plt.ylabel("Error")
-        plt.minorticks_on()
-        plt.grid(True, which="both", linewidth = 0.2)
-        plt.plot(timeAxis,errorAxis, linewidth="2")
+        # plt.subplot(2,1,2)
+        # plt.xlabel("Time (s)")
+        # plt.ylabel("Error")
+        # plt.minorticks_on()
+        # plt.grid(True, which="both", linewidth = 0.2)
+        # plt.plot(timeAxis,errorAxis, linewidth="2")
 
+
+        # plt.plot(x_waypoint,y_waypoint)
         plt.show()
-
-
     except:
         print("Did not connect to Carla server")
 
